db307c1d65701a35ca13c4ec9086fa5562575694
Oleg Konan <olegkonanff@gmail.com>
2023-07-14 18:12:33 +0400

    feat: getting categories

--- server/server.go
+++ server/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -17,14 +19,18 @@ func main() {
 
 	app := fiber.New()
 
-	app.Use("/api", func (c *fiber.Ctx) error {
-		c.Cookie(&fiber.Cookie{ SameSite: fiber.CookieSameSiteLaxMode })
-		return c.Next()
-	})
-
 	app.Use(logger.New())
 
 	app.Get("/api/categories", func(c *fiber.Ctx) error {
+		type Category struct {
+			id int
+			name string
+			picture_url sql.NullString
+			created_at time.Time
+		}
+
+		var categories []Category
+
 		rows, err := db.Query("SELECT * FROM categories")
 
 		if (err != nil) {
@@ -32,7 +38,20 @@ func main() {
 			return c.Status(fiber.StatusInternalServerError).SendString("Can't get categories from DB")
 		}
 
-		return c.JSON(rows)
+		defer rows.Close()
+
+		for rows.Next() {
+			var category Category
+			err := rows.Scan(&category.id, &category.name, &category.picture_url, &category.created_at)
+
+			if (err != nil) {
+				return c.Status(fiber.StatusInternalServerError).SendString("Can't get category from rows")
+			}
+
+			categories = append(categories, category)
+		}
+
+		return c.JSON(categories)
 	})
 
 	app.Get("/api/words", func(c *fiber.Ctx) error {
