bfeccbe81b842d9c7af2927a2b24eccfd8931c6b
Oleg Konan <olegkonanff@gmail.com>
2023-08-08 16:06:15 +0400

    feat: remove client

--- server/server.go
+++ server/server.go
@@ -6,17 +6,13 @@ import (
 	"server/router"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/favicon"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
 
-	app.Use(favicon.New())
-	app.Static("/", "./static")
-
 	router.SetupRoutes(app)
 
-	log.Fatal(app.Listen("localhost:8000"))
+	log.Panic(app.Listen("localhost:8000"))
 }
