a48a89d5262082865f2b9e1c941cd95b89b92261
Aleh Konan <olegkonanff@gmail.com>
2023-08-28 10:47:47 +0400

    dialog component

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,7 +1,19 @@
+import { useDialog } from '../hooks/useDialog';
+
 export const App = () => {
+  const dialog = useDialog();
+
   return (
     <div>
-      <p>App</p>
+      <button className="button" type="button" onClick={dialog.open}>
+        Open modal
+      </button>
+      <dialog ref={dialog.ref}>
+        <p>Dialog content</p>
+        <button className="button" type="button" onClick={dialog.close}>
+          Close
+        </button>
+      </dialog>
     </div>
   );
 };
