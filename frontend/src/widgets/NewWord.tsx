c9d879bf853382716cbf4b5c2eb5c2f6870acd8c
Aleh Konan <olegkonanff@gmail.com>
2023-09-02 16:45:12 +0400

    feat: form for new word

--- frontend/src/widgets/NewWord.tsx
+++ frontend/src/widgets/NewWord.tsx
@@ -1,16 +1,49 @@
+import { FormEvent } from 'react';
+
 import { Button } from '../components/Button';
 import { Dialog } from '../components/Dialog';
 import { useDialog } from '../hooks/useDialog';
 
+const formFields = {
+  word: 'word',
+  translation: 'translation',
+} as const;
+
 export const NewWordWidget = () => {
   const dialog = useDialog();
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    const entries = Object.values(formFields).map((field) => [
+      field,
+      e.currentTarget[field]?.value,
+    ]);
+    console.log(Object.fromEntries(entries));
+  };
 
   return (
     <div>
       <Button onClick={dialog.open}>New word</Button>
       <Dialog ref={dialog.ref}>
-        <p>Add new word</p>
-        <Button onClick={dialog.close}>Close</Button>
+        <h3 className="text-xl font-bold">Add new word</h3>
+        <form method="dialog" onSubmit={onSubmit} className="grid gap-2">
+          <label htmlFor="word">New word</label>
+          <input
+            id="word"
+            name={formFields.word}
+            className="border p-2"
+            required
+          />
+          <label htmlFor="translation">Translation</label>
+          <input
+            id="translation"
+            name={formFields.translation}
+            className="border p-2"
+          />
+          <div className="flex gap-4">
+            <Button type="submit">Submit</Button>
+            <Button onClick={dialog.close}>Close</Button>
+          </div>
+        </form>
       </Dialog>
     </div>
   );
